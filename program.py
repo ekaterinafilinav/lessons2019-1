272a1dacc94958d9b7168125c50b7f605bb363d1
LokirTreg <57491076+LokirTreg@users.noreply.github.com>
2019-11-07 17:11:17 +0300

    Update program.py

--- program.py
+++ program.py
@@ -3,7 +3,7 @@ def main():
     print('And I change it every day')
     print('Again')
     print('UFO came and added this line')
-
+    print('Gorno Hero')
 
 if __name__ == '__main__':
     main()
